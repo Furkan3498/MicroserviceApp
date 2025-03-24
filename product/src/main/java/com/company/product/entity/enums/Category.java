b266a1d796aedd0b9579b365a78628b675d83d38
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-03-25 01:01:49 +0300

    Product

---
+++ product/src/main/java/com/company/product/entity/enums/Category.java
@@ -0,0 +1,16 @@
+package com.company.product.entity.enums;
+
+public enum Category {
+    Electronic,
+    Moda,
+    Ev_Yasam_Kirtasiye,
+    Oto_Bahce_Yapi_Market,
+    Toys,
+    Sport_Outdoor,
+    Kozmetik_KisiselBakim,
+    Supermarket_PetShop,
+    Kitap,
+    Film,
+    Music,
+    Hobby,
+}
