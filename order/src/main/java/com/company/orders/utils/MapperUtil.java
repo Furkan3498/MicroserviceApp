6b6bf273245445b92831b9996fd57215e37e17d5
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-03-28 03:36:07 +0300

    OrderRefactorMapperUtil

---
+++ order/src/main/java/com/company/orders/utils/MapperUtil.java
@@ -0,0 +1,18 @@
+package com.company.orders.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
+
+public enum MapperUtil {
+
+    MAPPER_UTIL;
+
+    public <T> T map(InputStream source, Class<T> target){
+        try {
+            return  new ObjectMapper().readValue(source,target);
+        }catch (Exception exception){
+            throw new IllegalArgumentException(exception.getMessage());
+        }
+    }
+}
